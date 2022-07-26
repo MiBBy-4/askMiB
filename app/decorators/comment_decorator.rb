82c4f593aecd4a05d27259c828cf8b41118f644a
MiBBy-4 <maksim.762373maks@bk.ru>
2022-07-27 02:36:34 +0300

    fix: error message only for necessery comment

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -2,4 +2,10 @@ class CommentDecorator < ApplicationDecorator
   delegate_all
 
   decorates_association :user
+
+  def for?(commentable)
+    commentable = commentable.object if commentable.decorated?
+    
+    commentable == self.commentable
+  end
 end
