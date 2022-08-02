2f45f4e10150dff8fa6650996beb9ef73c1c0907
MiBBy-4 <maksim.762373maks@bk.ru>
2022-08-02 17:05:25 +0300

    feat: implement user and admin::user policies on all actions

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,21 @@
+class UserPolicy < ApplicationPolicy
+  def index?
+    false
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.guest?
+  end
+
+  def update?
+    record == user
+  end
+
+  def destroy?
+    false
+  end
+end
